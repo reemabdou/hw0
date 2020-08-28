da1eccccf0f8e6b4a77ac6844895b7da599c0e1d
reemabdou <rpa5285@psu.edu>
2020-08-28 20:46:51 +0000

    float

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 TCelsius = float(input("Enter temperature in celsius: "))
-TFahrenheit = int(TCelsius * 9/5 + 32)
+TFahrenheit = float(TCelsius * 9/5 + 32)
 print(str(TCelsius) + "° in Celsius is equivalent to " + str(TFahrenheit) + "° Fahrenheit.")
