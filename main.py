017f304be938b9b529c4f17322740380d8b5b2df
reemabdou <rpa5285@psu.edu>
2020-08-28 17:26:43 +0000

    .1

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 TCelsius = float(input("Enter temperature in celsius: "))
 TFahrenheit = (TCelsius * 9/5) + 32
-print('%.2f° Celsius is: %0.2f° Fahrenheit' %(TCelsius, TFahrenheit))
+print('%.1f° Celsius is: %0.1f° Fahrenheit' %(TCelsius, TFahrenheit))
