d6c35cda484e2945a7f63ed9572f7932359c1e33
reemabdou <rpa5285@psu.edu>
2020-08-28 17:28:40 +0000

    equivalent

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 TCelsius = float(input("Enter temperature in celsius: "))
 TFahrenheit = (TCelsius * 9/5) + 32
-print('%.1f° Celsius is: %0.1f° Fahrenheit' %(TCelsius, TFahrenheit))
+print('%.1f° Celsius is equivalent to %0.1f° Fahrenheit.' %(TCelsius, TFahrenheit))
